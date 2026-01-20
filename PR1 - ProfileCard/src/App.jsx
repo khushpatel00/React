31e9d715e5ed340aadd5c84c2b8534b19c63a6c7
khushpatel00 <patelxkhush@gmail.com>
2026-01-20 16:45:16 +0530

    fixed: card layout responsiveness

--- PR1 - ProfileCard/src/App.jsx
+++ PR1 - ProfileCard/src/App.jsx
@@ -21,11 +21,11 @@ function App() {
       </div>
 
 
-      <div className='flex relative justify-center mt-10 gap-10 showData'>
+      <div className='flex relative flex-wrap max-w-8/12 mx-auto justify-center mt-10 gap-10 showData'>
         {data?.map((item, index) => (
           <ProfileCard key={index} name={item.name} role={item.role} imageUrl={item.imageUrl} linkedinUrl={item.linkedinUrl} githubUrl={item.githubUrl} />
         ))}
-        <div className="absolute top-0 right-0 sm:right-0 md:right-0 lg:right-0 xl:right-1/6 2xl:right-1/5  -translate-1/2">
+        <div className="absolute top-0 right-0 sm:right-0 md:right-0 lg:right-0 -translate-1/2">
           <img className='w-32 h-32 rotate-45 translate-y-1/4' src="/arrow (1).png" alt="" srcSet="" />
           <p className='relative -translate-y-full translate-x-1/5 -rotate-3'>Hover Here</p>
         </div>
