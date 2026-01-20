d1a6a36d379fb639ee1b8087f2eca7fc44084f0f
khushpatel00 <patelxkhush@gmail.com>
2026-01-20 20:51:17 +0530

    init, added: Navbar, Landing Page

---
+++ FramerMotion - Ochi/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Components/Navbar.jsx'
+import Landing from './Components/Landing.jsx'
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Landing />
+    </div>
+  )
+}
+
+export default App
