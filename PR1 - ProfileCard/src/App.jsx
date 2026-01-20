93ab52cd77931d77d00618aaed7ab3a8f68c0cda
khushpatel00 <patelxkhush@gmail.com>
2026-01-20 16:40:51 +0530

    added: arrow icon for navigation

--- PR1 - ProfileCard/src/App.jsx
+++ PR1 - ProfileCard/src/App.jsx
@@ -21,10 +21,14 @@ function App() {
       </div>
 
 
-      <div className='flex justify-center mt-10 gap-10 showData'>
+      <div className='flex relative justify-center mt-10 gap-10 showData'>
         {data?.map((item, index) => (
           <ProfileCard key={index} name={item.name} role={item.role} imageUrl={item.imageUrl} linkedinUrl={item.linkedinUrl} githubUrl={item.githubUrl} />
         ))}
+        <div className="absolute top-0 right-0 sm:right-0 md:right-0 lg:right-0 xl:right-1/6 2xl:right-1/5  -translate-1/2">
+          <img className='w-32 h-32 rotate-45 translate-y-1/4' src="/arrow (1).png" alt="" srcSet="" />
+          <p className='relative -translate-y-full translate-x-1/5 -rotate-3'>Hover Here</p>
+        </div>
       {console.log(data)}
 
       </div>
