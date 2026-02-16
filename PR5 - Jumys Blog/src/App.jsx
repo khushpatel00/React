1fbd5ed6c67608644bf1195c797972ba2dbbb11f
khushpatel00 <patelxkhush@gmail.com>
2026-02-16 13:12:44 +0530

    Integrated: header and footer

--- PR5 - Jumys Blog/src/App.jsx
+++ PR5 - Jumys Blog/src/App.jsx
@@ -1,11 +1,13 @@
 import Blog from './Components/Blog/Blog'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
 function App() { 
 
   return (
     <>
         {/* header placeholder */}
-        <div style={{ 
+        {/* <div style={{ 
             height: '108px',
             width: '95vw',
             position: 'sticky',
@@ -13,10 +15,10 @@ function App() {
             zIndex: 9999,
             backgroundColor: 'transparent',
             borderRadius: '0 0 40px 40px'
-        }} className='mx-auto'>  
-            
-        </div>
+        }} className='mx-auto'></div> */}
+      <Header />
       <Blog />
+      <Footer />
     </>
   )
 }
